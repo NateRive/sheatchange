e8ed6a7ccfd1d0650c929287e10afaa79f1562f4
NateRive <naterive1125@gmail.com>
2018-04-30 16:38:02 +0900

    Improved algorythm2

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -20,11 +20,10 @@ $(function() {
             clearInterval(id);
             $(tmp2).parent().addClass("empty").removeClass("filled");
           }
-        }, 3000);
+        }, 1000);
       } else if ($(".empty").length > 1) {
         //空いている席が２つ以上の場合の処理
         var restTimes = []
-        console.log($(".empty"));
 
         $(".empty").each(function(index, element) {
           var ifPrev = $(element).parent().prev();
@@ -45,30 +44,32 @@ $(function() {
               restTimes.push({ restTime: [ Number(prevNumber), 10 ], emptySeat: Number(tableId) });
             } else if (ifNext.length == 1) {
               var nextNumber = $(ifNext).find(".row__part__box--amount").text();
-              var tableId = $(element).parent().next().data("id");
+              var tableId = $(element).parent().data("id");
               restTimes.push({ restTime: [ Number(nextNumber), 10 ], emptySeat: Number(tableId) });
             }
           }
           });
-
-          console.log(restTimes);
           var max = restTimes[0].emptySeat;
+          var toCompared = restTimes[0].restTime
           for (var i = 0; i < restTimes.length - 1; i++) {
             function whichIsBig(a,b) {
               if (a[0] < b[0]) {
                 return true;
               } else if (a[0] > b[0]) {
                 return false;
+                //１つ目の引数の配列の最小値が２つ目の引数の最小値よりも小さければ、座る席の候補を後者に譲る
               } else if (a[0] == b[0]) {
                 if (a[1] < b[1]) {
                   return true;
                 } else if (a[1] >= b[1]) {
                   return false;
-                }
+                }//最小値で決着がつかなければ、最大値を比べ、大きい方を隣席としてもつ席を、座る候補とする。
               }
             }
-            if (whichIsBig(restTimes[i].restTime, restTimes[i+1].restTime)) {
+            if (whichIsBig(toCompared, restTimes[i+1].restTime)) {
               max = restTimes[i+1].emptySeat;
+              toCompared = restTimes[i+1].restTime
+              //Trueが返り値ならば、座る席の候補を変え、その席を新たな比較基準とする。
             }
           }
           var answer = $(`[data-id=${max}]`).find(".row__part__box");
